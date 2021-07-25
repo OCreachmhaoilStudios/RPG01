233afdbbd1f2ce8132c1b4ad4577e1fc24185113
Matthew Crowell <57077970+OCreachmhaoilStudios@users.noreply.github.com>
2021-07-25 17:30:58 -0500

    improve UpdateAnimator method in Mover.cs

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,38 +1,43 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace Movement
 {
-    private static readonly int ForwardSpeed = Animator.StringToHash("forwardSpeed");
-    private Camera _camera;
-    private NavMeshAgent _navMeshAgent;
-
-    // Start is called before the first frame update
-    private void Start()
+    public class Mover : MonoBehaviour
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-        _camera = Camera.main;
-    }
+        private static readonly int ForwardSpeed = Animator.StringToHash("forwardSpeed");
+        private Animator _animator;
+        private Camera _camera;
+        private NavMeshAgent _navMeshAgent;
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetMouseButton(0)) MoveToPoint();
-        UpdateAnimator();
-    }
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _camera = Camera.main;
+            _animator = GetComponent<Animator>();
+        }
 
-    private void UpdateAnimator()
-    {
-        var velocity = _navMeshAgent.velocity;
-        var localVelocity = transform.InverseTransformDirection(velocity);
-        var speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat(ForwardSpeed, speed);
-    }
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetMouseButton(0)) MoveToPoint();
+            UpdateAnimator();
+        }
 
-    private void MoveToPoint()
-    {
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        var hasHit = Physics.Raycast(ray, out var hit);
-        if (hasHit) _navMeshAgent.SetDestination(hit.point);
+        private void UpdateAnimator()
+        {
+            var velocity = _navMeshAgent.velocity;
+            var localVelocity = transform.InverseTransformDirection(velocity);
+            var speed = localVelocity.z;
+            _animator.SetFloat(ForwardSpeed, speed);
+        }
+
+        private void MoveToPoint()
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            var hasHit = Physics.Raycast(ray, out var hit);
+            if (hasHit) _navMeshAgent.SetDestination(hit.point);
+        }
     }
 }
