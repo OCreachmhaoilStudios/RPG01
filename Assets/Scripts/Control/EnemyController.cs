6c9d93b40e0f4f21c8a413b58e043ed42e7e66c3
Matthew Crowell <57077970+OCreachmhaoilStudios@users.noreply.github.com>
2021-08-10 06:52:13 -0500

    add enemy range check against player

--- Assets/Scripts/Control/EnemyController.cs
+++ Assets/Scripts/Control/EnemyController.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+namespace Control
 {
-    // Start is called before the first frame update
-    private void Start()
+    public class EnemyController : MonoBehaviour
     {
-    }
+        [SerializeField] private float chaseDistance = 5f;
+        private GameObject _player;
 
-    // Update is called once per frame
-    private void Update()
-    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _player = GameObject.FindWithTag("Player");
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            EngagePlayer();
+        }
+
+        private void EngagePlayer()
+        {
+            if (Vector3.Distance(_player.transform.position, gameObject.transform.position) <= chaseDistance)
+                print(gameObject.name + " should chase " + _player.name);
+        }
     }
 }
